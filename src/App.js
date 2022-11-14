9f7f253c4d5d98fa3bb69f6150f71b3965166cf1
Vishalivishali <you@example.com>
2022-11-14 14:07:38 +0530

    Included style.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import { React } from 'react';
 import './App.scss';
+import getCompensations from './data';
 
-const App = () =>
-	<div className="App" role="App">
-		Ready to start.
+const getOutput = ({ name, amount }) =>
+	`${ name } got ${ amount }.`;
+
+const App = () => {
+	const compensations = getCompensations();
+	const lines = compensations.map((compensation, key) =>
+		<div
+			key={ key }
+			style={ {
+				background: 'Red',
+				border: '10px solid blue',
+				marginTop: '5px',
+			} }
+		>
+			{ getOutput(compensation)}
+		</div>);
+
+	// eslint-disable-next-line no-console
+	console.log({ compensations, lines });
+
+	return <div>
+		<h1>Compensations</h1>
+		<div> { lines }</div>
 	</div>;
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,12 @@
+const getCompensations = () => [
+	{
+		name: 'A',
+		amount: 200,
+	},
+	{
+		name: 'B',
+		amount: 300,
+	},
+];
+
+export default getCompensations;
